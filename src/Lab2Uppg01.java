e2560e333440266fe64c085a9df5a8290c4bd535
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-17 14:47:13 +0200

    Uppgift #1 påbörjad.

---
+++ src/Lab2Uppg01.java
@@ -0,0 +1,19 @@
+/**
+ *  DV017A :: Grundläggande programmering i Java
+ *  860224 Jonas Sjöberg
+ *  Högskolan i Gävle
+ *  tel12jsg@student.hig.se
+ *
+ *  Labb #2
+ *  Uppgift 1
+ */
+
+
+public class Lab2Uppg01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
