6c2de6afc78ec897d229f923d35c76d5a993e1c8
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-24 08:01:39 +0200

    Lägg till "formatter on/off tags"
    Styr Eclipse kodformateringsfunktion.
    
    @formatter:off      avaktivera autoformatering
    @formatter:on       aktivera   autoformatering
    
    Ctrl+shift+F för att formatera aktuell fil.
    Använder egen "profile" 'jonas.xml' för kodstil.

--- src/Lab2Uppg01.java
+++ src/Lab2Uppg01.java
@@ -11,21 +11,21 @@ public class Lab2Uppg01
      * programmets exekvering. Även om antal sekunder per minut med största
      * sannolikhet alltid kommer att vara 60 så är det "good practice" att
      * undvika att "hårdkoda magiska siffror" direkt i koden. */
-    private static final int SECONDS_PER_MINUTE = 60;
-    private static final int MINUTES_PER_HOUR   = 60;
-    private static final int HOURS_PER_DAY      = 24;
+    private static final int    SECONDS_PER_MINUTE = 60;
+    private static final int    MINUTES_PER_HOUR   = 60;
+    private static final int    HOURS_PER_DAY      = 24;
 
     /* Konstant variabel QUERY skrivs ut då användaren ombeds mata in sekunder. */
-    private static final String QUERY = 
-            "Var vänlig ange antal sekunder att omvandla till minuter och timmar.";
+    private static final String QUERY              = "Var vänlig ange antal sekunder att omvandla till minuter och timmar.";
 
     public static void main(String[] args)
     {
+        @formatter:off  // Avaktivera Eclipse auto-formatering
         UserInputFilter filter = new UserInputFilter();
 
         int sekunder = filter.getPositiveInt(QUERY);
-        int minuter = numberOfMinutesForSeconds(sekunder);
-        int timmar = numberOfHoursForMinutes(minuter);
+        int minuter  = numberOfMinutesForSeconds(sekunder);
+        int timmar   = numberOfHoursForMinutes(minuter);
 
         System.out.println("________________________________________");
         System.out.println("");
@@ -34,12 +34,15 @@ public class Lab2Uppg01
         System.out.println("            " + minuter + " minuter");
         System.out.println("            " + timmar + (timmar > 1 ? " timmar" : " timme"));
         System.out.println("________________________________________");
+        @formatter:on   // Aktivera Eclipse auto-formatering
     }
 
     /**
      * Beräknar antal minuter för ett givet antal sekunder.
-     * @param seconds       antal sekunder att beräkna
-     * @return              antal minuter
+     * 
+     * @param seconds
+     *            antal sekunder att beräkna
+     * @return antal minuter
      */
     static int numberOfMinutesForSeconds(int seconds)
     {
@@ -48,8 +51,10 @@ public class Lab2Uppg01
 
     /**
      * Beräknar antal timmar för ett givet antal minuter.
-     * @param minutes       antal minuter att beräkna
-     * @return              antal timmar
+     * 
+     * @param minutes
+     *            antal minuter att beräkna
+     * @return antal timmar
      */
     static int numberOfHoursForMinutes(int minutes)
     {
