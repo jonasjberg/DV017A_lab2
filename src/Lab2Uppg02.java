8d50c83f30cd83cc08534c539becf268ddce8484
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-24 07:38:34 +0200

    Lägg till 'Lab2Uppg02.java'.

---
+++ src/Lab2Uppg02.java
@@ -0,0 +1,14 @@
+/**
+ *  DV017A :: Grundläggande programmering i Java
+ *  860224 Jonas Sjöberg
+ *  Högskolan i Gävle
+ *  tel12jsg@student.hig.se
+ *
+ *  Labb #2
+ *  
+ */
+
+public class Lab2Uppg02
+{
+
+}
