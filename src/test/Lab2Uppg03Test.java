75ef38fff653740b086e0526c58566f4defe2280
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-25 19:07:59 +0200

    Lägg till JUnit test för Uppgift #3.

---
+++ src/test/Lab2Uppg03Test.java
@@ -0,0 +1,24 @@
+/**
+ * DV017A :: Grundläggande programmering i Java
+ * 860224 Jonas Sjöberg
+ * Högskolan i Gävle
+ * tel12jsg@student.hig.se
+ * 
+ * Labb #2    Uppgift 3 JUnit test
+ */
+
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Lab2Uppg03Test
+{
+
+    @Test
+    public void testMellerstaTalet()
+    {
+        fail("Not yet implemented");
+    }
+
+}
