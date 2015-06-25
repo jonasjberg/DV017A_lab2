1b31aa505476a799ce9095e5479345975401d32b
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-25 20:40:56 +0200

    Lägg till enkelt JUnit test case för Uppgift #3.

--- src/test/Lab2Uppg03Test.java
+++ src/test/Lab2Uppg03Test.java
@@ -9,30 +9,37 @@
 
 package test;
 
-import static org.junit.Assert.*;
+import main.Lab2Uppg03;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class Lab2Uppg03Test
 {
-
+    private static int OSORTERADE_TAL[];
+    Lab2Uppg03  uppg03;
+    
     @Before
     public void setUp() throws Exception
     {
-
+        OSORTERADE_TAL = new int[3];
+        OSORTERADE_TAL[0] = 47;
+        OSORTERADE_TAL[1] = 89;
+        OSORTERADE_TAL[2] = 3;
+        uppg03 = new Lab2Uppg03();
     }
 
     @After
     public void tearDown() throws Exception
     {
-
+        uppg03 = null;
     }
 
     @Test
     public void testMellerstaTalet()
     {
-        fail("Not yet implemented");
+        assertEquals(47, uppg03.mellerstaTalet(OSORTERADE_TAL));
     }
 
 }
