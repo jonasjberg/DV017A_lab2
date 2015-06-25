f857e313d740a5690901e70b02e21ad0c83b0a46
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-25 19:21:53 +0200

    Tom testklass, lämplig mall.

--- src/test/Lab2Uppg03Test.java
+++ src/test/Lab2Uppg03Test.java
@@ -10,10 +10,24 @@
 package test;
 
 import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class Lab2Uppg03Test
 {
+
+    @Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    }
 
     @Test
     public void testMellerstaTalet()
