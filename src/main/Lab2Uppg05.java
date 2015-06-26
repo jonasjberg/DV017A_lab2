d54523f00c0d7687b2873977ea7f35a0eb014c6d
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-27 00:31:30 +0200

    Fixa Uppgift #5.

--- src/main/Lab2Uppg05.java
+++ src/main/Lab2Uppg05.java
@@ -11,52 +11,25 @@ package main;
 
 public class Lab2Uppg05
 {
-    private static int count_a = 0;
-    private static int count_e = 0;
-    private static int count_i = 0;
-    private static int count_o = 0;
-    private static int count_u = 0;
-    private static int count_y = 0;
-
     public static void main(String[] args)
     {
         UserInputFilter filter = new UserInputFilter();
 
-        String text = filter.getString("Mata in strängen: ");
+        /* Hämta textsträng från användaren. */
+        String text = filter.getString("Skriv in den sträng du vill leta i: ");
+        /* Saknar skydd mot att användaren matar in annat än en bokstav. */
+        char ch = filter.getString("Vilken bokstav vill du räkna: ").charAt(0);
+        
+        int chCount = 0;
 
-        /* Gå igenom textsträngen 'text' ett tecken i taget och gör jämförelsen
-         * i en switch-sats. Öka respektive räknare om tecknet matchar. */
+        /* Gå igenom textsträngen 'text' ett tecken i taget och jämför tecknet
+         * vid position 'i' med 'ch'. Öka räknaren om tecknet matchar. */
         for (int i = 0; i < text.length(); i++) {
-            switch (text.charAt(i)) {
-            case 'a':
-                count_a++;
-                break;
-            case 'e':
-                count_e++;
-                break;
-            case 'i':
-                count_i++;
-                break;
-            case 'o':
-                count_o++;
-                break;
-            case 'u':
-                count_u++;
-                break;
-            case 'y':
-                count_y++;
-                break;
-            default:
-                break;
-            }
+            if (text.charAt(i) == ch) chCount++;
         }
 
         /* Skriv ut resultatet. */
-        System.out.println("I strängen finns: " + count_a + " st a:n");
-        System.out.println("                  " + count_e + " st e:n");
-        System.out.println("                  " + count_i + " st i:n");
-        System.out.println("                  " + count_o + " st o:n");
-        System.out.println("                  " + count_u + " st u:n");
-        System.out.println("                  " + count_y + " st y:n");
+        System.out.println(
+                "Strängen innehåller " + chCount + " st " + ch + ":n.");
     }
 }
