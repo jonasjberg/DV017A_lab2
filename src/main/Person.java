73463b51a45facdbf38405bd70e2e3c3efea6431
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-28 00:50:52 +0200

    Uppdatera 'Person.java'.

--- src/main/Person.java
+++ src/main/Person.java
@@ -12,12 +12,18 @@ package main;
 public class Person
 {
     private String namn;
-    private int personnummer;
+    private long personnummer;
     private String adress;
     private int alder;
 
-   
-    public Person(String namn, int ID, String adress, int alder)
+    /**
+     * Konstruktor för klassen 'Person' som representerar en person.
+     * @param namn      personens namn
+     * @param ID        personens personnummer
+     * @param adress    personens adress
+     * @param alder     personens ålder
+     */
+    public Person(String namn, long ID, String adress, int alder)
     {
         this.namn = namn;
         this.personnummer = ID;
@@ -25,51 +31,77 @@ public class Person
         this.alder = alder;
     }
     
+    /**
+     * Byter namn på personen.
+     * @param namn  personens nya namn
+     */
     public void byterNamn(String namn)
     {
         this.namn = namn;
     }
     
+    /**
+     * Ändrar personens adress.
+     * @param adress    personens nya adress
+     */
+    public void byterAdress(String adress)
+    {
+        this.adress = adress;
+    }
+    
+    /**
+     * Gör personen ett år äldre.
+     */
     public void fyllerAr()
     {
         alder++;
     }
     
+    /**
+     * Hämta personens namn
+     * @return      personens namn
+     */
     public String hamtaNamn()
     {
         return namn;
     }
     
-    public int hamtaPersnr()
+    /**
+     * Hämta personens personnummer
+     * @return      personens personnummer
+     */
+    public long hamtaPersnr()
     {
         return personnummer;
     }
     
+    /**
+     * Hämta personens ålder
+     * @return      personens ålder
+     */
     public int hamtaAlder()
     {
         return alder;
     }
     
+    /**
+     * Hämta personens adress
+     * @return      personens adress
+     */
     public String hamtaAdress()
     {
         return adress;
     }
     
+    /**
+     * Returnerar objektets data i "human-readable" format
+     */
     public String toString()
     {
-        return ("Namn: " + this.namn + "    Personnummer: " + this.personnummer
-                + "    Adress:" + this.adress + "    Ålder: " + this.alder);
+        // Använder '\n' newlines trots att det inte är särskilt portabelt ..
+        return ("\nNamn:         " + this.namn
+              + "\nPersonnummer: " + this.personnummer
+              + "\nAdress:       " + this.adress
+              + "\nÅlder:        " + this.alder + "\n");
     }
-
-    public static void main(String[] args)
-    {
-
-
-
-    }
-    
-    
-    
-    
-
 }
