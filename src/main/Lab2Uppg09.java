a3a69a55c156f26c02029359f652c7c3e4839267
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-28 01:02:47 +0200

    Lägg till Uppgift #9.

---
+++ src/main/Lab2Uppg09.java
@@ -0,0 +1,20 @@
+/**
+ *  DV017A :: Grundläggande programmering i Java
+ *  860224 Jonas Sjöberg
+ *  Högskolan i Gävle
+ *  tel12jsg@student.hig.se
+ *
+ *  Labb #2    Uppgift 9
+ */
+
+package main;
+
+public class Lab2Uppg09
+{
+
+    public static void main(String[] args)
+    {
+
+    }
+
+}
